1f2f9c2ea44cff556fc74c184562bcd0b3d32aa9
2881099 <2881099@qq.com>
2020-12-07 20:59:04 +0800

    update version

--- src/FreeSql.Cloud/FreeSqlTransaction.cs
+++ src/FreeSql.Cloud/FreeSqlTransaction.cs
@@ -18,7 +18,7 @@ namespace FreeSql
         {
             _orm = fsql;
             _resolveTran = resolveTran;
-            //Ado = new ScopeTransactionAdo(_orm.Ado as AdoProvider, resolveTran);
+            Ado = new ScopeTransactionAdo(_orm.Ado as AdoProvider, resolveTran);
         }
 
         public static FreeSqlTransaction Create(IFreeSql fsql, Func<DbTransaction> resolveTran)
@@ -29,24 +29,25 @@ namespace FreeSql
             return Create(scopedfsql._orm, resolveTran);
         }
 
-        //class ScopeTransactionAdo : AdoProvider
-        //{
-        //    AdoProvider _ado;
-        //    public ScopeTransactionAdo(AdoProvider ado, Func<DbTransaction> resolveTran) : base(ado.DataType, null, null)
-        //    {
-        //        _ado = ado;
-        //        base.ResolveTransaction = resolveTran;
-        //        base.ConnectionString = ado.ConnectionString;
-        //        base.SlaveConnectionStrings = ado.SlaveConnectionStrings;
-        //        base.Identifier = ado.Identifier;
-        //    }
-        //    public override object AddslashesProcessParam(object param, Type mapType, ColumnInfo mapColumn) => _ado.AddslashesProcessParam(param, mapType, mapColumn);
-        //    public override DbCommand CreateCommand() => _ado.CreateCommand();
-        //    public override DbParameter[] GetDbParamtersByObject(string sql, object obj) => _ado.GetDbParamtersByObject(sql, obj);
-        //    public override void ReturnConnection(IObjectPool<DbConnection> pool, Object<DbConnection> conn, Exception ex) => _ado.ReturnConnection(pool, conn, ex);
-        //}
-        //public IAdo Ado { get; private set; }
-        public IAdo Ado => throw new NotSupportedException("IFreeSql 对象被重写，支持事务且只能使用 CRUD 方法");
+        class ScopeTransactionAdo : AdoProvider
+        {
+            AdoProvider _ado;
+            public ScopeTransactionAdo(AdoProvider ado, Func<DbTransaction> resolveTran) : base(ado.DataType, null, null)
+            {
+                _ado = ado;
+                base.ResolveTransaction = resolveTran;
+                base.ConnectionString = ado.ConnectionString;
+                base.SlaveConnectionStrings = ado.SlaveConnectionStrings;
+                base.Identifier = ado.Identifier;
+                base.MasterPool = ado.MasterPool;
+                base._util = ado._util;
+            }
+            public override object AddslashesProcessParam(object param, Type mapType, ColumnInfo mapColumn) => _ado.AddslashesProcessParam(param, mapType, mapColumn);
+            public override DbCommand CreateCommand() => _ado.CreateCommand();
+            public override DbParameter[] GetDbParamtersByObject(string sql, object obj) => _ado.GetDbParamtersByObject(sql, obj);
+            public override void ReturnConnection(IObjectPool<DbConnection> pool, Object<DbConnection> conn, Exception ex) => _ado.ReturnConnection(pool, conn, ex);
+        }
+        public IAdo Ado { get; private set; }
         public IAop Aop => throw new NotSupportedException("IFreeSql 对象被重写，支持事务且只能使用 CRUD 方法");
         public ICodeFirst CodeFirst => throw new NotSupportedException("IFreeSql 对象被重写，支持事务且只能使用 CRUD 方法");
         public IDbFirst DbFirst => throw new NotSupportedException("IFreeSql 对象被重写，支持事务且只能使用 CRUD 方法");
