1a22587b3b822e2bbadb64f017401b602393568c
2881099 <2881099@qq.com>
2021-01-13 19:25:10 +0800

    update demo

---
+++ examples/net50_webapi_idlebus/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace net50_webapi_idlebus.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet]
+        public object Get([FromServices] IFreeSql fsql)
+        {
+            fsql.Change("db2").Ado.ExecuteConnectTest();
+            fsql.Change("db3").Ado.ExecuteConnectTest();
+            return "";
+        }
+    }
+}
---
+++ examples/net50_webapi_idlebus/Startup.cs
@@ -0,0 +1,49 @@
+using FreeSql;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace net50_webapi_idlebus
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var fsql = new MultiFreeSql();
+            fsql.Register("db1", () => new FreeSqlBuilder().UseConnectionString(DataType.Sqlite, "data source=db1.db").Build());
+            fsql.Register("db2", () => new FreeSqlBuilder().UseConnectionString(DataType.Sqlite, "data source=db2.db").Build());
+            fsql.Register("db3", () => new FreeSqlBuilder().UseConnectionString(DataType.Sqlite, "data source=db3.db").Build());
+
+            services.AddSingleton<IFreeSql>(fsql);
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
